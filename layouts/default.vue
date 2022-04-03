3e423bd66387b5f1a961800f81ea28f503e738ac
Stoo <stoo.goff@gmail.com>
2022-04-03 17:48:16 +0100

    Updated home page and added default layout.

---
+++ layouts/default.vue
@@ -0,0 +1,31 @@
+<template>
+	<div>
+		<section class="relative py-16 bg-gray-300">
+			<div class="container mx-auto md:px-4 max-w-screen-lg">
+				<div class="relative flex flex-col py-8 bg-white w-full mb-6 shadow-xl sm:rounded-lg pb-8">
+					<Nuxt />
+				</div>
+			</div>
+		</section>
+		<message-view v-if="hasText">{{ text }}</message-view>
+	</div>
+</template>
+<script>
+
+import { message } from '~/state'
+
+export default {
+	name: 'DefaultLayout',
+
+	computed: {
+		text() {
+			return message.getMessage()
+		},
+
+		hasText() {
+			return message.hasMessage()
+		}
+	}
+}
+
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,33 @@
 <template>
-  <Tutorial/>
+	<div>
+		<h1>AEGEAN</h1>
+		<ul>
+			<li>
+				<h2>Characters</h2>
+				<p>Create a new character and view existing ones.</p>
+			</li>
+			<li>
+				<h2>Cities</h2>
+				<p>Create a new city and view existing ones.</p>
+			</li>
+			<li>
+				<h2>Adversaries</h2>
+				<p>Find adversaries to fight.</p>
+			</li>
+			<li>
+				<h2>Factions</h2>
+				<p>View factions.</p>
+			</li>
+			<li>
+				<h2>GM Screen</h2>
+				<p>View GM reference material.</p>
+			</li>
+		</ul>
+	</div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+	name: 'IndexPage'
 }
 </script>
