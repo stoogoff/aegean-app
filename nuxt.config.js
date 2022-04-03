cf953c83e56d61147ef801e7b5420fe2ce177f84
Stoo <stoo.goff@gmail.com>
2022-04-03 17:40:39 +0100

    Base components

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,15 @@ export default {
 	// Target: https://go.nuxtjs.dev/config-target
 	target: 'static',
 
+  env: {
+		apiKey: process.env.FIREBASE_API_KEY,
+		authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+		projectId: process.env.FIREBASE_PROJECT_ID,
+		storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+		messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+		appId: process.env.FIREBASE_APP_ID,
+  },
+
 	// Global page headers: https://go.nuxtjs.dev/config-head
 	head: {
 		titleTemplate: '%s | Aegean',
