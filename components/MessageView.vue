28fa859fb98092867462f57a7ee9db4baf5e2ab2
Stoo <stoo.goff@gmail.com>
2025-08-23 21:43:08 +0100

    Removing nuxt

--- components/MessageView.vue
+++
@@ -1,9 +0,0 @@
-<template>
-	<slide-up>
-		<div class="fixed bottom-2 left-4 right-4 flex justify-center z-50">
-			<div class="p-2 shadow-lg w-full max-w-xl alert success">
-				<slot />
-			</div>
-		</div>
-	</slide-up>
-</template>
